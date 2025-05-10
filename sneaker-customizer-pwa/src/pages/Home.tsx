bfa81127ae6de611d305ddffbdd0a3d6d81f0945
Berari Mihai-Dan <berari.mihaidan@gmail.com>
2025-05-10 20:49:28 +0300

    10

--- sneaker-customizer-pwa/src/pages/Home.tsx
+++ sneaker-customizer-pwa/src/pages/Home.tsx
@@ -26,7 +26,7 @@ const Home: React.FC = () => {
             <motion.div {...headTextAnimation}>
               <h1 className="home-title">
                 Your Shoes
-                <br className="line-break" /> Your Way 9
+                <br className="line-break" /> Your Way 10
               </h1>
             </motion.div>
 
