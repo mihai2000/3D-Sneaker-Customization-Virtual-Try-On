e553ff0a32812c1a5c8574a9f11309527d8e2f0d
mihai2000 <berari.mihaidan@gmail.com>
2025-05-16 12:49:37 +0300

    design update to the cart

--- sneaker-customizer-pwa/src/components/Cart/EmptyCart.tsx
+++ sneaker-customizer-pwa/src/components/Cart/EmptyCart.tsx
@@ -8,7 +8,7 @@ export default function EmptyCart() {
 	return (
 		<Box
 			sx={{
-				minHeight: "100vh",
+				// minHeight: "100vh",
 				display: "flex",
 				alignItems: "center",
 				justifyContent: "center",
