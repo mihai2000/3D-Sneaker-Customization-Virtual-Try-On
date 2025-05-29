cc337bdd8c759de6f0a9d8c304eb19d2c79f1f52
Berari Mihai-Dan <berari.mihaidan@gmail.com>
2025-05-29 22:37:16 +0300

    selector on arviewer

--- sneaker-customizer-pwa/src/pages/AR/ARViewer.tsx
+++ sneaker-customizer-pwa/src/pages/AR/ARViewer.tsx
@@ -1,63 +1,3 @@
-// import { useEffect } from 'react';
-// import { useSearchParams } from 'react-router-dom';
-// import QRCode from 'react-qr-code';
-
-// declare global {
-//   interface Window {
-//     deepar: any;
-//   }
-// }
-
-// // Reliable mobile detection
-// const isMobile = () => {
-//   return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-//     navigator.userAgent
-//   );
-// };
-
-// export default function ARViewer() {
-//   const [params] = useSearchParams();
-//   const productId = params.get('product');
-
-//   useEffect(() => {
-//     if (isMobile()) {
-//       const script = document.createElement('script');
-//       script.src = '/deepar/deepar.js';
-//       script.onload = async () => {
-//         const DeepAR = window.deepar;
-//         const deepAR = new DeepAR({
-//           licenseKey: import.meta.env.VITE_DEEPAR_SDK_KEY,
-//           canvas: document.getElementById('deepar-canvas'),
-//           libPath: '/deepar/',
-//         });
-//         await deepAR.downloadFaceTrackingModel();
-//         await deepAR.initialize();
-//         deepAR.switchEffect(0, 'slot', `/effects/${productId}/`);
-//       };
-//       document.body.appendChild(script);
-//     }
-//   }, [productId]);
-
-//   if (!isMobile()) {
-//     const mobileURL = `${window.location.origin}/collection/shoes?product=${productId}&mode=ar`;
-//     return (
-//       <div>
-//         <h3>Scan to Try On!</h3>
-//         <QRCode value={mobileURL} size={256} />
-//       </div>
-//     );
-//   }
-
-//   return (
-//     <canvas
-//       id="deepar-canvas"
-//       width="640"
-//       height="480"
-//       style={{ width: '100%' }}
-//     />
-//   );
-// }
-
 import { useEffect, useRef, useState } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import { initialize } from 'deepar';
@@ -154,16 +94,21 @@ export default function ARViewer() {
   const qrUrl = `${window.location.origin}/collection/shoes?product=${selectedShoe.id}&mode=ar`;
 
   return isMobile ? (
-    <>
+    <div className="mobile-container">
       <canvas
         id="deepar-canvas"
         width="380"
         height="720"
         style={{ width: '100%', margin: '0' }}
       />
-      <p className="ar-mobile-label">{selectedShoe.name}</p>
-      <ShoeSelector onSelect={handleSelect} selectedShoeId={selectedShoe.id} />
-    </>
+      <div className='mobile-sub-container'>
+        <p className="ar-mobile-label">{selectedShoe.name}</p>
+        <ShoeSelector
+          onSelect={handleSelect}
+          selectedShoeId={selectedShoe.id}
+        />
+      </div>
+    </div>
   ) : (
     <Layout>
       <div className="tryon-container">
